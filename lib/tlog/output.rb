c1a448d0a75e5f8c73d96b08dfe705c1e2fd6467
Chris Wendel <chriwend@umich.edu>
2013-06-16 21:25:53 -0400

    Tabs to spaces

--- lib/tlog/output.rb
+++ lib/tlog/output.rb
@@ -1,33 +1,33 @@
 
 class Tlog::Output
 
-	attr_accessor :stdout
-	attr_accessor :stderr
+  attr_accessor :stdout
+  attr_accessor :stderr
 
-	def initialize(stdout,stderr)
-		@stdout = stdout
-		@stderr = stderr
-	end
+  def initialize(stdout,stderr)
+    @stdout = stdout
+    @stderr = stderr
+  end
 
-	def error(err)
-		@stderr.puts err
-	end
+  def error(err)
+    @stderr.puts err
+  end
 
-	def line(out)
-		@stdout.puts out
-		true
-	end
+  def line(out)
+    @stdout.puts out
+    true
+  end
 
-	def line_yellow(out)
-		@stdout.puts out.yellow
-	end
+  def line_yellow(out)
+    @stdout.puts out.yellow
+  end
 
-	def line_red(out)
-		@stdout.puts out.red
-	end
+  def line_red(out)
+    @stdout.puts out.red
+  end
 
-	def line_blue(out)
-		@stdout.puts out.blue
-	end
+  def line_blue(out)
+    @stdout.puts out.blue
+  end
 
 end
